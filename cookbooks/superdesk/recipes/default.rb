3dc2958db7db97eb3d89b3bae53ef2ad1e186fdd
petr.jasek <petr.jasek@sourcefabric.org>
2013-02-21 15:29:22 +0100

    Clone Superdesk into Ally repo.

--- cookbooks/superdesk/recipes/default.rb
+++ cookbooks/superdesk/recipes/default.rb
@@ -8,30 +8,33 @@ package "git"
 package "python3"
 package "default-jre"
 
-execute "git clone git://github.com/sourcefabric/Ally-Py.git /vagrant/ally-py" do
+execute "git clone --depth=10 git://github.com/sourcefabric/Ally-Py.git /vagrant/ally-py" do
     creates "/vagrant/ally-py"
 end
 
-execute "git clone git://github.com/sourcefabric/Superdesk.git /vagrant/superdesk" do
-    creates "/vagrant/superdesk"
+execute "git clone --depth=10 git://github.com/sourcefabric/Superdesk.git /vagrant/ally-py/superdesk" do
+    creates "/vagrant/ally-py/superdesk"
 end
 
-directory "/vagrant/ally-py/superdesk"
-
-execute "cp -r /vagrant/superdesk/* /vagrant/ally-py/superdesk/" do
-    creates "/vagrant/ally-py/superdesk/README"
+execute "update-ally" do
+    command "git pull"
+    cwd "/vagrant/ally-py"
 end
 
-execute "/vagrant/ally-py/superdesk/build-eggs" do
+execute "update-superdesk" do
+    command "git pull"
     cwd "/vagrant/ally-py/superdesk"
-    creates "/vagrant/ally-py/superdesk/distribution/components"
+end
+
+execute "build-eggs" do
+    command "./build-eggs"
+    cwd "/vagrant/ally-py/superdesk"
 end
 
 app_path = "/vagrant/ally-py/superdesk/distribution/application.py"
-cwd_path = File.dirname(app_path)
 
 execute "python3 %s -dump" % app_path do
-    cwd cwd_path
+    cwd File.dirname(app_path)
 end
 
 service "superdesk" do
