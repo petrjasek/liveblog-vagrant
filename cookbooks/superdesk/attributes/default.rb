3e8fe009a65811403743595f5946b39c4ef2a87c
petr.jasek <petr.jasek@sourcefabric.org>
2013-02-25 15:17:18 +0100

    Switch repos to devel branches.

---
+++ cookbooks/superdesk/attributes/default.rb
@@ -0,0 +1,5 @@
+default.ally.branch = "devel"
+default.ally.repo = "git://github.com/sourcefabric/Ally-Py.git"
+
+default.superdesk.branch = "devel"
+default.superdesk.repo = "git://github.com/sourcefabric/Superdesk.git"
--- cookbooks/superdesk/recipes/default.rb
+++ cookbooks/superdesk/recipes/default.rb
@@ -1,29 +1,18 @@
-apt_today = "/var/log/apt-%s" % Date.today.to_s
-execute "apt-get update && touch %s" % apt_today do
-    # update apt once a day
-    creates apt_today
+execute "apt-get update" do
+    action :nothing
+    not_if "which git && which python3 && which java"
 end
 
 package "git"
 package "python3"
 package "default-jre"
 
-execute "git clone --depth=10 git://github.com/sourcefabric/Ally-Py.git /vagrant/ally-py" do
+execute "git clone --depth=3 --branch #{node.ally.branch} #{node.ally.repo} /vagrant/ally-py" do
     creates "/vagrant/ally-py"
 end
 
-execute "git clone --depth=10 git://github.com/sourcefabric/Superdesk.git /vagrant/ally-py/superdesk" do
+execute "git clone --depth=3 --branch #{node.superdesk.branch} #{node.superdesk.repo} /vagrant/ally-py/superdesk" do
     creates "/vagrant/ally-py/superdesk"
-end
-
-execute "update-ally" do
-    command "git pull"
-    cwd "/vagrant/ally-py"
-end
-
-execute "update-superdesk" do
-    command "git pull"
-    cwd "/vagrant/ally-py/superdesk"
 end
 
 execute "build-eggs" do
@@ -33,11 +22,11 @@ end
 
 app_path = "/vagrant/ally-py/superdesk/distribution/application.py"
 
-execute "python3 %s -dump" % app_path do
+execute "python3 #{app_path} -dump" do
     cwd File.dirname(app_path)
 end
 
 service "superdesk" do
     action :start
-    start_command "python3 %s &" % app_path
+    start_command "python3 #{app_path} &"
 end
