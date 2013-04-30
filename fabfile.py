ff439d40885503b65c3ca36c97242f1100a93158
petr.jasek <petr.jasek@sourcefabric.org>
2013-04-30 11:56:45 +0200

    Don't hardcode lowercase superdesk to work with default clone.

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,8 @@ from fabric.api import *
 from fabric.context_managers import shell_env
 
 cwd = os.path.dirname(os.path.realpath(__file__))
-runtime = os.path.join(cwd, 'superdesk', 'distribution', 'application.py')
+app = glob.glob(os.path.join('[Ss]uperdesk', 'distribution', 'application.py')).pop()
+runtime = os.path.join(cwd, app)
 
 plugins = [os.path.dirname(x) for x in glob.glob(os.path.join(cwd, '*', '*', '*', 'setup.py'))]
 paths = ':'.join(plugins)
