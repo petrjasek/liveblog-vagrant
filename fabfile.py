bf638de697dc75a254e1b5e2fa02a4f4a0508bda
petr.jasek <petr.jasek@sourcefabric.org>
2013-08-14 10:32:34 +0200

    Keep plugin properties during clean

--- fabfile.py
+++ fabfile.py
@@ -8,6 +8,7 @@ from fabric.context_managers import shell_env
 cwd = os.path.dirname(os.path.realpath(__file__))
 app = glob.glob(os.path.join('[Ss]uperdesk', 'distribution', 'application.py')).pop()
 runtime = os.path.join(cwd, app)
+workspace = os.path.join(cwd, os.path.dirname(app), 'workspace')
 
 plugins = [os.path.dirname(x) for x in glob.glob(os.path.join(cwd, '*', '*', '*', 'setup.py'))]
 paths = ':'.join(plugins)
@@ -22,14 +23,21 @@ def run():
     with shell_env(PYTHONPATH=pythonpath):
         local('python3.2 %s' % runtime)
 
-def clean():
-    workspace = os.path.join(cwd, os.path.dirname(app), 'workspace')
-
+def clean_workspace():
     try:
         shutil.rmtree(workspace)
     except OSError:
         pass
 
-    properties = glob.glob(os.path.join(os.path.dirname(workspace), '*.properties'))
+def clean_properties():
+    properties = [
+        os.path.join(os.path.dirname(workspace), 'application.properties'),
+        os.path.join(os.path.dirname(workspace), 'distribution.properties'),
+    ]
+
     for propfile in properties:
         os.remove(propfile)
+
+def clean():
+    clean_workspace()
+    clean_properties()
